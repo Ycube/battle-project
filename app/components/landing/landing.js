724ca8b32a67f7698fa04961722f4950ed7d1b5d
Yale Yuen <yaleyyuen@gmail.com>
2016-02-29 01:22:42 -0800

    feat(client): add styling

--- app/components/landing/landing.js
+++ app/components/landing/landing.js
@@ -1,8 +1,8 @@
 angular.module('lolStats.landing', [])
 
   .controller('LandingController', ['$scope', '$location', 'landingService' ,function ($scope, $location, landingService) {
-    $scope.userName = '';
-    $scope.season = 'SEASON2016';
+    $scope.userName = 'rondomvp';
+    $scope.season = 'SEASON2014';
     
     $scope.sendUsername = function(){
       landingService.sendData($scope.userName, $scope.season)
